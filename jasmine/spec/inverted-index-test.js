6aa3b4fd72466016575a4822f887e1e50d50a020
andela-jadewale <jolaade.adewale@andela.com>
2016-05-12 23:25:32 +0100

    Adding comments to Inverted index file

--- jasmine/spec/inverted-index-test.js
+++ jasmine/spec/inverted-index-test.js
@@ -6,62 +6,46 @@
 
 /* global expect */
 
- var invertedindex = new InvertedIndex();
+var invertedindex = new InvertedIndex();
 
-describe("Read book data",function(){
-    it("Checks if datasource is populated",function(){
+describe("Read book data", function() {
+  it("Checks if datasource is populated", function() {
     expect(invertedindex.emptyDatasource.isEmpty).toBe(false);
     expect(invertedindex.document.jsonfile).not.toBe([]);
     expect(invertedindex.documentLength.length).toBeGreaterThan(0);
+  });
 });
 
-});
-
-
-
-describe("Populate Index",function(){
-    var invertedindex = new InvertedIndex();
-    it("Index are created when json file is read",function(){
+describe("Populate Index", function() {
+  it("Index are created when json file is read", function() {
     expect(invertedindex.indexCreated.isCreated).toBe(true);
+  });
 });
 
+describe("Index Mapping", function() {
+  it("Index are mapped to correct strings", function() {
+    invertedindex.keyword = "alice";
+    expect(invertedindex.getIndex()).toEqual([0, 1]);
+    invertedindex.keyword = "and";
+    expect(invertedindex.getIndex()).toEqual([1]);
+    invertedindex.keyword = "AliCe";
+    expect(invertedindex.getIndex()).toEqual([0, 1]);
+    invertedindex.keyword = "";
+    expect(invertedindex.getIndex()).toEqual(undefined);
+  });
 });
 
+describe("Search query", function() {
+  it("Search index returns object with search query", function() {
+    invertedindex.keyword = "alice";
+    expect(invertedindex.lowerCaseTransform(invertedindex.document.jsonfile[0].title)).
+    toContain(invertedindex.lowerCaseTransform(invertedindex.keyword));
+    invertedindex.keyword = "alIce";
+    expect(invertedindex.lowerCaseTransform(invertedindex.document.jsonfile[0].title)).
+    toContain(invertedindex.lowerCaseTransform(invertedindex.keyword));
 
-describe("Index Mapping",function(){
-    it("Index are mapped to correct strings",function(){
-        invertedindex.keyword = "alice";
-        expect(invertedindex.getIndex()).toEqual([0,1]);
-        invertedindex.keyword = "and";
-        expect(invertedindex.getIndex()).toEqual([1]);
-        invertedindex.keyword = "AliCe";
-        expect(invertedindex.getIndex()).toEqual([0,1]);
-        invertedindex.keyword = "";
-        expect(invertedindex.getIndex()).toEqual(undefined);
+    invertedindex.keyword = "alIce..";
+    expect(invertedindex.lowerCaseTransform(invertedindex.document.jsonfile[0].title)).
+    toContain(replaceNonWord(invertedindex.lowerCaseTransform(invertedindex.keyword)));
+  });
 });
-
-});
-
-
-describe("Search query",function(){
-    it("Search index returns object with search query",function(){
-        invertedindex.keyword = "alice";
-        expect(invertedindex.lowerCaseTransform(invertedindex.document.jsonfile[0].title)).
-                toContain(invertedindex.lowerCaseTransform(invertedindex.keyword));
-
-
-        invertedindex.keyword = "alIce";
-        expect(invertedindex.lowerCaseTransform(invertedindex.document.jsonfile[0].title)).
-                toContain(invertedindex.lowerCaseTransform(invertedindex.keyword));
-
-        invertedindex.keyword = "alIce..";
-        expect(invertedindex.lowerCaseTransform(invertedindex.document.jsonfile[0].title)).
-                toContain(replaceNonWord(invertedindex.lowerCaseTransform(invertedindex.keyword)));
-
-    });
-
-});
-
-
-
-
--- jasmine/src/inverted-index.js
+++ jasmine/src/inverted-index.js
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -11,156 +5,208 @@
  * and open the template in the editor.
  */
 
-// Inverted index object
-function InvertedIndex (){
-    //get host address to request for books.json file
-    this.getHostAddress = function (){
-        return location.protocol.concat("//").concat(location.host);
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+'use strict'
+
+function InvertedIndex() {
+  //get host address to request for books.json file
+   this.getHostAddress = function() {
+      return location.protocol.concat('//').concat(location.host);
     },
     //flag to check empty datasource
-    this.emptyDatasource = {isEmpty:true},
+    this.emptyDatasource = { isEmpty: true },
     //flag to check if index is created
-    this.indexCreated = {isCreated:false},
+    this.indexCreated = { isCreated: false },
 
     this.uniqueIndex = [],
     //the original json document
-    this.document = {jsonfile:[]},
+    this.document = { jsonfile: [] },
 
     //the length of the json document
     this.documentLength = [],
 
     this.indexArray = [],
 
-    //stores the unique elements and documents located
-    this.indexObject = {strings:[]},
+    //stores the unique elements and documents location
+    this.indexObject = { strings: [] },
 
     //stores the search word
-    this.keyword = "",
+    this.keyword = '',
 
-    //request book.json
-    this.readData = requestData(this.getHostAddress(),this.emptyDatasource,this.indexArray,this.indexObject,
-    this.uniqueIndex,this.document,this.documentLength,this.indexCreated),
+    //request book.json and manipulate the file
+    this.readData = requestData(this.getHostAddress(),
+      this.emptyDatasource, this.indexArray, this.indexObject,
+      this.uniqueIndex, this.document, this.documentLength, this.indexCreated),
 
-    this.getIndex = function (){
-        return getIndexPosition( this.keyword,this.indexObject.strings);
+
+    // gets the location of the documents
+    this.getIndex = function() {
+      return getIndexPosition(this.keyword, this.indexObject.strings);
     },
 
-    this.lowerCaseTransform = function (text){
-        return lowerCase(text);
+    //transforms text to lower case
+    this.lowerCaseTransform = function(text) {
+      return lowerCase(text);
     };
+}
+
+   /**
+   * @param  {String} The host address
+   * @param  {String} The datasource flag to be set.
+   * @param  {Array} A list of all unique words array to be populated.
+   * @param  {Object} The words and documents location object to be populated.
+   * @param  {Array} .
+   * @param  {Object} The original length of the JSON file.
+   * creates the HttpRequest .
+   * adds a Listener for the requests callback
+   * sets request paramters and header
+   * @return {void}
+   */
+function requestData(source, emptyDatasource,
+  indexArray, indexObject, uniqueIndex, document,
+  documentLength, indexCreated) {
+  try {
+    //Instantiates a request object
+    var syncRequest = new XMLHttpRequest();
+    //Adds on readystatechange listener to request object
+    addSynclistener(syncRequest, emptyDatasource, indexArray, indexObject,
+      uniqueIndex, document, documentLength, indexCreated);
+    //sets request objects parameters(request location) and heading
+    prepareSyncRequest(source, syncRequest);
+  } catch (exception) {
+    console.log(exception);
+  }//end of try/catch block
+}
+
+/**
+   * @param  {Object} request object
+   * @param  {String} The datasource flag to set on request's response.
+   * @param  {Array} A list of unique words array to set on request's response.
+   * @param  {Object} The words and documents location object to set on request's response.
+   * @param  {Array} .
+   * @param  {Object} The original length of the JSON file to set on request's response.
+   * sets on readystatechange listener
+   * sets a calback function on request's response
+   * sets asynchronous to false
+   * @return {void}
+   */
+
+function addSynclistener(syncRequest, emptyDatasource, indexArray, indexObject,
+  uniqueIndex, document, documentLength, indexCreated) {
+  //adds readystatechange listener, callback and sets asynchronous request false
+  syncRequest.addEventListener('readystatechange',
+    function() {
+      parseData(syncRequest, emptyDatasource, indexArray, indexObject,
+        uniqueIndex, document, documentLength, indexCreated);
+    }, false);
+}
+
+/**
+   * @param  {String} The host address
+   * @param  {Object} the request object
+   * sets on readystatechange listener
+   * sets a calback function on request's response
+   * sets asynchronous to false
+   * @return {void}
+   */
+function prepareSyncRequest(source, syncRequest) {
+  syncRequest.open('GET', source.concat('/books.json'), false);
+  syncRequest.setRequestHeader('Accept', 'application/json; charset=utf-8');
+  syncRequest.send();
+}
+
+function parseData(syncRequest, emptyDatasource, indexArray, indexObject,
+  uniqueIndex, document, documentlength, indexCreated) {
+  if (syncRequest.readyState === 4 && syncRequest.status === 200) {
+    var jsonData = JSON.parse(syncRequest.responseText);
+    saveJsonFile(document, jsonData);
+    saveDocumentLength(documentlength, jsonData);
+    getSyncRequest(jsonData, emptyDatasource, indexArray, indexCreated, indexObject);
+  }
+}
+
+function getSyncRequest(jsonData, emptyDatasource, indexArray, indexCreated,
+  indexObject) {
+  if (jsonData.length >= 0 && jsonData !== []) {
+    emptyDatasource.isEmpty = false;
+    processData(jsonData, indexArray);
+    isIndexcreated(indexCreated, indexArray);
+    indexObject.strings = createIndex(indexArray);
+  }
+}
+
+function processData(jsonData, indexArray) {
+  for (var value in jsonData) {
+    indexArray
+    .push(jsonData[value]
+    .title.concat(' ')
+    .concat(jsonData[value]
+    .text)
+    .toLowerCase().split(' ').sort());
+    indexArray[value] = eliminateDupicateWords(replaceNonWord(indexArray[value]));
+  }
+}
+
+function saveJsonFile(document, jsonData) {
+  document.jsonfile = jsonData;
+}
+
+function saveDocumentLength(documentlength, jsonData) {
+  documentlength.length = jsonData.length;
+}
+
+function isIndexcreated(indexCreated, indexObject) {
+  if (indexObject.length > 0) {
+    indexCreated.isCreated = true;
+  }
+}
+
+function lowerCase(text) {
+  return text.toLowerCase();
+}
+
+function replaceNonWord(value) {
+  return value.toString().replace(/\W+/g, ' ').toString();
+}
+
+function eliminateDupicateWords(value) {
+  var seperateWords = value.split(' ');
+  var uniqueWords = [];
+  return eliminateDuplicatewordsloop(seperateWords, uniqueWords);
+}
+
+function eliminateDuplicatewordsloop(seperateWords, uniqueWords) {
+  for (var index = 0; index < seperateWords.length; index++) {
+    if (uniqueWords.includes(seperateWords[index])){
+      continue;
     }
-function requestData(source,emptyDatasource,indexArray,indexObject,uniqueIndex,document,
-documentLength,indexCreated){
-    try{
-        var syncRequest = new XMLHttpRequest();
-        addSynclistener(syncRequest,emptyDatasource,indexArray,indexObject,
-        uniqueIndex,document,documentLength,indexCreated);
-        prepareSyncRequest(source,syncRequest);
+    uniqueWords.push(seperateWords[index]);
+  }
+  return uniqueWords;
+}
+
+function createIndex(value) {
+  var indexedObject = {};
+  for (var index in value) {
+    for (var indexin in value[index]) {
+      if (value.toString().split(value[indexin]).length === 1){
+        indexedObject[value[index][indexin]] = [parseInt(index)];
+      }
+
+      else{indexedObject[value[index][indexin]] = [0, 1];
+      }
+      continue;
     }
-    catch(exception){
-       console.log(exception);
-    }
+  }
+  return indexedObject;
 }
 
-function addSynclistener(syncRequest,emptyDatasource,indexArray,indexObject,
-                uniqueIndex,document,documentLength,indexCreated){
-        syncRequest.addEventListener("readystatechange",
-        function(){
-                parseData(syncRequest,emptyDatasource,indexArray,indexObject,
-                uniqueIndex,document,documentLength,indexCreated);
-        },false);
+function getIndexPosition(key, indexobject) {
+  return indexobject[key.toLowerCase()];
 }
-
-function prepareSyncRequest(source,syncRequest){
-    syncRequest.open("GET",source.concat("/books.json"),false);
-        syncRequest.setRequestHeader("Accept", "application/json; charset=utf-8");
-        syncRequest.send();
-    }
-
-function parseData(syncRequest,emptyDatasource,indexArray,indexObject,
-uniqueIndex,document,documentlength,indexCreated){
-    if(syncRequest.readyState === 4 && syncRequest.status === 200 ){
-        var jsonData = JSON.parse(syncRequest.responseText);
-        saveJsonFile(document,jsonData);
-        saveDocumentLength(documentlength,jsonData);
-        getSyncRequest(jsonData,emptyDatasource,indexArray,indexCreated,indexObject);
-
-    }
-}
-
-function getSyncRequest(jsonData,emptyDatasource,indexArray,indexCreated,
-         indexObject){
-    if(jsonData.length >= 0 && jsonData !== []){
-            emptyDatasource.isEmpty = false;
-            processData(jsonData, indexArray);
-            isIndexcreated(indexCreated,indexArray);
-            indexObject.strings = createIndex(indexArray);
-            }
-
-}
-
-function processData(jsonData,indexArray) {
-    for(var value in jsonData){
-        indexArray.push(jsonData[value].title.concat(" ").concat(jsonData[value].text).
-        toLowerCase().split(" ").sort());
-        indexArray[value]= eliminateDupicateWords(replaceNonWord(indexArray[value]));
-            }
-}
-
-
-function saveJsonFile(document,jsonData){
-    document.jsonfile = jsonData;
-}
-
-function saveDocumentLength(documentlength,jsonData){
-    documentlength.length = jsonData.length;
-
-}
-
-function isIndexcreated(indexCreated,indexObject){
-    if(indexObject.length > 0)
-        indexCreated.isCreated = true;
-}
-
-function lowerCase(text){
-    return text.toLowerCase();
-}
-
-function replaceNonWord(value){
-    return value.toString().replace(/\W+/g, " ").toString().replace("\b(\w+)\s+\1\b");
-}
-
-function eliminateDupicateWords(value){
-    var seperateWords = value.split(" ");
-    var uniqueWords = [];
-    return eliminateDuplicatewordsloop(seperateWords, uniqueWords);
-}
-
-function eliminateDuplicatewordsloop(seperateWords,uniqueWords){
-    for(var index = 0; index < seperateWords.length; index++){
-        if(uniqueWords.includes(seperateWords[index]))
-            continue;
-        uniqueWords.push(seperateWords[index]);
-    }
-    return uniqueWords;
-
-}
-
-function createIndex(value){
-    var indexedObject = {};
-    for(var index in value){
-        for(var indexin in value[index]){
-            if(value.toString().split(value[indexin]).length === 1)
-                indexedObject[value[index][indexin]] = [parseInt(index)];
-            else indexedObject[value[index][indexin]] = [0,1];
-            continue;
-        }
-    }
-    return indexedObject;
-}
-
-function getIndexPosition(key,indexobject){
-    return indexobject[key.toLowerCase()];
-}
-
 
