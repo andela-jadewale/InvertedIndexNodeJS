02020f4ae43558e0b163dc81e140cce994b4a665
andela-jadewale <jolaade.adewale@andela.com>
2016-05-12 15:27:40 +0100

    Name change

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ var http = require("http");
 var express = require("express");
 var httpApp = express();
 httpApp.use(express.static(__dirname + '/jasmine'));
-
 httpApp.get("/",function (req,res){
     res.sendFile(__dirname+ "/" + "jasmine/SpecRunner.html");
 
