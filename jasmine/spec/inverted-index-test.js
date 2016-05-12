14e1114d1e594158fbd9f2cdb7f75f94dc12e504
andela-jadewale <jolaade.adewale@andela.com>
2016-05-13 00:13:05 +0100

    comments to test suite

--- jasmine/spec/inverted-index-test.js
+++ jasmine/spec/inverted-index-test.js
@@ -5,31 +5,45 @@
  */
 
 /* global expect */
+'use strict'
 
 var invertedindex = new InvertedIndex();
 
 describe("Read book data", function() {
   it("Checks if datasource is populated", function() {
+    //check is empty datasource flag is empty
     expect(invertedindex.emptyDatasource.isEmpty).toBe(false);
+    //check taht datasource file is not empty array
     expect(invertedindex.document.jsonfile).not.toBe([]);
+
+    //check document length > 0
     expect(invertedindex.documentLength.length).toBeGreaterThan(0);
   });
 });
 
 describe("Populate Index", function() {
   it("Index are created when json file is read", function() {
+    //check index created is true
     expect(invertedindex.indexCreated.isCreated).toBe(true);
   });
 });
 
 describe("Index Mapping", function() {
   it("Index are mapped to correct strings", function() {
+
+    //set keyword to alice and test index
     invertedindex.keyword = "alice";
     expect(invertedindex.getIndex()).toEqual([0, 1]);
+
+    //set keyword to and then test index
     invertedindex.keyword = "and";
     expect(invertedindex.getIndex()).toEqual([1]);
+
+    //add upper case A and test index
     invertedindex.keyword = "AliCe";
     expect(invertedindex.getIndex()).toEqual([0, 1]);
+
+    //search empty word and test index
     invertedindex.keyword = "";
     expect(invertedindex.getIndex()).toEqual(undefined);
   });
@@ -37,13 +51,19 @@ describe("Index Mapping", function() {
 
 describe("Search query", function() {
   it("Search index returns object with search query", function() {
+
+    //test search with alice
     invertedindex.keyword = "alice";
     expect(invertedindex.lowerCaseTransform(invertedindex.document.jsonfile[0].title)).
     toContain(invertedindex.lowerCaseTransform(invertedindex.keyword));
+
+      //test search upper case
     invertedindex.keyword = "alIce";
     expect(invertedindex.lowerCaseTransform(invertedindex.document.jsonfile[0].title)).
     toContain(invertedindex.lowerCaseTransform(invertedindex.keyword));
 
+
+      //test search with non words
     invertedindex.keyword = "alIce..";
     expect(invertedindex.lowerCaseTransform(invertedindex.document.jsonfile[0].title)).
     toContain(replaceNonWord(invertedindex.lowerCaseTransform(invertedindex.keyword)));
