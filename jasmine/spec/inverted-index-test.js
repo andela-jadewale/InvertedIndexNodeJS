19398663a1947c4b1cc7fed4b2a254b5c1c92deb
Jolaade Adewale <jolaade.adewale@andela.com>
2016-06-03 13:34:51 +0100

    transform loops to filter,map and reduce

--- jasmine/spec/inverted-index-test.js
+++ jasmine/spec/inverted-index-test.js
@@ -1,104 +1,97 @@
 (function () {
-   'use strict';
-var invertedIndex;
-var documentLength;
+  'use strict';
+  var invertedIndex;
+  var documentLength;
 
-describe('Initialises Index object', function () {
-  beforeEach( function (){
-    invertedIndex = new Index();
-    invertedIndex.createIndex(invertedIndex.getHostAddress() + '/jasmine/books.json');
-  }) ;
+  describe('Initialises Index object', function () {
+    beforeEach( function (){
+      invertedIndex = new Index();
+      invertedIndex.createIndex(invertedIndex.getHostAddress() + '/jasmine/books.json');
+    }) ;
 
-describe('Read book data', function () {
-  it('Checks if datasource is populated ', function (done) {
-    setTimeout (function () {
-      expect(invertedIndex.emptyDatasource.isEmpty).toBe(false);
-      expect(invertedIndex.jsonDocument.jsonFile).not.toBe([]);
-      // loops through  to assert json values are strings
-
-
-          //check string
+    describe('Read book data', function () {
+      it('Checks if datasource is populated ', function (done) {
+        setTimeout (function () {
+          expect(invertedIndex.emptyDatasource.isEmpty).toBe(false);
+          expect(invertedIndex.jsonDocument.jsonFile).not.toBe([]);
+          //asserts json file read are strings
           invertedIndex.jsonDocument.jsonFile.forEach(function(value) {
             value.map(function(obj){
-            expect(typeof obj.title).toEqual('string');
-            expect(typeof obj.text).toEqual('string');
+              expect(typeof obj.title).toEqual('string');
+              expect(typeof obj.text).toEqual('string');
             });
-
           });
-
           expect(invertedIndex.documentLength).toBeGreaterThan(0);
           expect(invertedIndex.documentLength).toEqual(2);
+          // assigns a value to documenlength which will be checked after second call to create index
           documentLength = invertedIndex.documentLength;
-           done();
+          done();
         },500);
+      });
     });
   });
-});
 
-describe('Populate Index', function () {
-  it('Index are created when json file is read', function () {
-    expect(invertedIndex.indexCreated.isCreated).toBe(true);
-    invertedIndex.createIndex(invertedIndex.getHostAddress()+'/jasmine/read.json');
-    it('Test book.json file is not overwritten ', function (done) {
-      setTimeout (function () {
-      // document length which shows create index did not overwrite previous
-      expect(invertedIndex.documentLength).toBeGreaterThan(2);
-      done();
-    }, 500);
+  describe('Populate Index', function () {
+    it('Index are created when json file is read', function () {
+      expect(invertedIndex.indexCreated.isCreated).toBe(true);
+      invertedIndex.createIndex(invertedIndex.getHostAddress()+'/jasmine/read.json');
+      it('Test book.json file is not overwritten ', function (done) {
+        setTimeout (function () {
+        // document length which shows create index did not overwrite previous
+          expect(invertedIndex.documentLength).toBeGreaterThan(2);
+          done();
+        }, 500);
+      });
     });
   });
-});
 
-describe('Index Mapping', function () {
-  it('Index are mapped to correct strings', function () {
-    expect(invertedIndex.getIndex().and).toEqual([0,1]);
-    expect(invertedIndex.getIndex().alice).toEqual([0]);
-    expect(invertedIndex.getIndex('alice')).toEqual([0]);
-    expect(invertedIndex.getIndex().seek).toEqual([1]);
-    expect(invertedIndex.getIndex().of).toEqual([0,1]);
-
-    it('Test read.json file ', function(done) {
-      setTimeout (function () {
-        // testing index mapping
+  describe('Index Mapping', function () {
+    it('Index are mapped to correct strings', function () {
       expect(invertedIndex.getIndex().and).toEqual([0,1]);
-      expect(invertedIndex.getIndex().concise).toEqual([3]);
-      expect(invertedIndex.getIndex().pocahontas).toEqual([2]);
-      expect(invertedIndex.getIndex().a).toEqual([0,1,2]);
-      done();
-    }, 500);
-  });});
-
-});
-
-describe('Search Index', function () {
-  it('Search index returns object with search query', function(done) {
-    setTimeout (function () {
-      expect(invertedIndex.searchIndex('and')).toEqual([0,1]);
-      expect(invertedIndex.searchIndex('alice')).toEqual([0]);
-      expect(invertedIndex.searchIndex('')).toEqual('');
-      var pastDate = new Date();
-      expect(invertedIndex.searchIndex('wonderland')).toEqual([0]);
-      expect(invertedIndex.searchIndex('alice and wonderland in'))
-        .toEqual([ [ 0 ], [ 0,1 ], [ 0 ], [ 0 ] ] );
-        // test complicated search works
-      expect(invertedIndex.searchIndex('Wonderland ?  . concise in '+
-        '/ pocahontas, seek'))
-        .toEqual([[ 0 ], [ 3 ], [ 0 ], [ 2 ], [ 1 ]] );
-        // test an array passed into the search works
-      expect(invertedIndex.searchIndex([['alice', 'and','wonderland', 'in']]))
-        .toEqual([ [ 0 ], [ 0,1 ], [ 0 ], [ 0 ] ] );
-      expect(invertedIndex.searchIndex(['wonderland'])).toEqual([0]);
-      expect(invertedIndex.searchIndex('wonderland')).toEqual([0]);
-      expect(invertedIndex.searchIndex('alice and wonderland in'))
-        .toEqual([ [ 0 ], [ 0,1 ], [ 0 ], [ 0 ] ] );
-      var presentDate = new  Date();
-      // test time to complete 7 search's will be less than 5 milliseconds
-      expect(presentDate.getMilliseconds() - pastDate.getMilliseconds() )
-      .toBeLessThan(5);
-      done();
-    }, 500);
-
+      expect(invertedIndex.getIndex().alice).toEqual([0]);
+      expect(invertedIndex.getIndex('alice')).toEqual([0]);
+      expect(invertedIndex.getIndex().seek).toEqual([1]);
+      expect(invertedIndex.getIndex().of).toEqual([0,1]);
+      it('Test read.json file ', function(done) {
+        setTimeout (function () {
+          // testing index mapping
+          expect(invertedIndex.getIndex().and).toEqual([0,1]);
+          expect(invertedIndex.getIndex().concise).toEqual([3]);
+          expect(invertedIndex.getIndex().pocahontas).toEqual([2]);
+          expect(invertedIndex.getIndex().a).toEqual([0,1,2]);
+          done();
+        }, 500);
+      });
+    });
   });
-});
 
-}());
+  describe('Search Index', function () {
+    it('Search index returns object with search query', function(done) {
+      setTimeout (function () {
+        expect(invertedIndex.searchIndex('and')).toEqual([0,1]);
+        expect(invertedIndex.searchIndex('alice')).toEqual([0]);
+        expect(invertedIndex.searchIndex('')).toEqual('');
+        var pastDate = new Date();
+        expect(invertedIndex.searchIndex('wonderland')).toEqual([0]);
+        expect(invertedIndex.searchIndex('alice and wonderland in'))
+          .toEqual([ [ 0 ], [ 0,1 ], [ 0 ], [ 0 ] ] );
+          // test complicated search works
+        expect(invertedIndex.searchIndex('Wonderland ?  . concise in '+
+          '/ pocahontas, seek'))
+          .toEqual([[ 0 ], [ 3 ], [ 0 ], [ 2 ], [ 1 ]] );
+          // test an array passed into the search works
+        expect(invertedIndex.searchIndex([['alice', 'and','wonderland', 'in']]))
+          .toEqual([ [ 0 ], [ 0,1 ], [ 0 ], [ 0 ] ] );
+        expect(invertedIndex.searchIndex(['wonderland'])).toEqual([0]);
+        expect(invertedIndex.searchIndex('wonderland')).toEqual([0]);
+        expect(invertedIndex.searchIndex('alice and wonderland in'))
+          .toEqual([ [ 0 ], [ 0,1 ], [ 0 ], [ 0 ] ] );
+        var presentDate = new  Date();
+        // test time to complete 7 search's will be less than 5 milliseconds
+        expect(presentDate.getMilliseconds() - pastDate.getMilliseconds() )
+          .toBeLessThan(5);
+        done();
+      }, 500);
+    });
+  });
+  }());
--- src/inverted-index.js
+++ src/inverted-index.js
@@ -8,14 +8,28 @@ function Index(){
   this.indexArray = [];
   this.indexObject = { data:[] };
 
+  /**
+   * gets host address
+   * @return {String} returns host address
+   */
   this.getHostAddress = function () {
     return location.protocol + '//' + location.host;
     };
 
+  /**
+   * sets filename to retrieve document
+   * @param  {String} fileName name of json file
+   * @return {String}  returns fileName
+   */
   this.file = function (fileName) {
     return fileName;
   };
 
+  /**
+   * gets the document located of searchword
+   * @param  {String} key search word
+   * @return {Array}  an array of documents located
+   */
   this.getIndex = function (key) {
     if(key !== undefined){
     // initiateRequest(key);
@@ -25,6 +39,11 @@ function Index(){
     return this.indexObject.data;
   };
 
+  /**
+   * searchs object for a word and returns the document located
+   * @param  {String} word search word
+   * @return {Array}    an array of documents located
+   */
   this.searchIndex = function (word) {
     return isValidData(word)?
     getIndexPosition(replaceNonWord(word), this.indexObject.data) : '';
@@ -33,9 +52,13 @@ function Index(){
   this.lowerCaseTransform = function (text) {
     return lowerCase(text);
   };
-
+  /**
+   * reads json file and saves words with document location in an object
+   * @param  {String} filepath a path to locate json file
+   * @return {Void}
+   */
   this.createIndex = function (filepath) {
-    return isValidData(filepath)? initiateRequest(filepath) : '';
+    isValidData(filepath)? initiateRequest(filepath) : '';
   };
 
   /**
@@ -81,6 +104,14 @@ function Index(){
     asyncRequest.send();
   }
 
+  /**
+   * @param  {Object} The request object
+   * gets the response text
+   * saves response to an object
+   * increases document length
+   * maps the response text to their documents
+   * @return {void}
+   */
   function callBack(asyncRequest) {
     if (asyncRequest.readyState === 4 && asyncRequest.status === 200) {
       var jsonData = JSON.parse(asyncRequest.responseText);
@@ -90,16 +121,26 @@ function Index(){
     }
   }
 
+  /**
+   * @param  {Object} jsonData The response text (json object)
+   * adds it to the existing documents
+   * @return {void}
+   */
   function saveJsonFile(jsonData) {
     _this.jsonDocument.jsonFile.push(jsonData);
   }
 
+  /**
+   * @param  {Object} jsonData The response text (json object)
+   * increases the flag which saves documents length
+   * @return {void}
+   */
   function saveDocumentLength( jsonData) {
     _this.documentLength += jsonData.length;
   }
 
   /**
-   * @param  {Object} books.json object
+   * @param  {Object} jsonData json object
    * sets empty data source flag to false
    * processes data recieved
    * sets create Index true
@@ -116,10 +157,10 @@ function Index(){
   }
 
   /**
-   * @param  {Object} books.json object
-   * populates the unique elements from jsonData and sorts them
+   * @param  {Object} jsonData json file
+   * converts to lower case and sorts jsondata
    * elimanates duplicate words and replaces non words
-   * re-initialises unique elements to sorted and unique words
+   * pushes unique words to array
    * @return {void}
    */
   function processAsyncData(jsonData) {
@@ -130,12 +171,19 @@ function Index(){
     });
 
   }
-
+  /**
+   * @param  {String} data words to be transformed
+   * transform data to lowercase , splits then sorts in ascending order
+   * @return {String}      the sorted lowercase data
+   */
   function transformAsyncData(data){
     return lowerCase(data).split(' ').sort();
   }
 
-  // sets iscreated to true
+  /**
+   * sets indexcreated flag to true
+   * @return {void} [description]
+   */
   function isIndexCreated() {
     if (_this.indexArray.length > 0) {
       _this.indexCreated.isCreated = true;
@@ -143,13 +191,13 @@ function Index(){
   }
 
   /**
-   * @param  {Array} The unique elements
+   * @param  {Array} uniqueWords The unique elements
    * loops through all elements and gets the document location from the index
    * @return {Object} the Indexed object
    */
-  function createIndex(document) {
+  function createIndex(uniqueWords) {
     var indexedObject = {};
-    document.forEach(function (docData,docIndex){
+    uniqueWords.forEach(function (docData,docIndex){
      docData.filter(function (words){
        (indexedObject[words] !== undefined)? indexedObject[words].push(docIndex)
         : indexedObject[words] = [docIndex];
@@ -162,67 +210,58 @@ function Index(){
   function lowerCase(text) {
     return text.toLowerCase();
   }
-
-  function replaceNonWord(value) {
-    return value.toString().replace(/\W+/g, ' ').toString();
+  /**
+   * @param  {String} word words
+   * replaces non alphabet characters using regex e.g ?*.,+
+   * @return {String} A word with only alphabets
+   */
+  function replaceNonWord(word) {
+    return word.toString().replace(/\W+/g, ' ').toString();
   }
 
   /**
-   * @param  {Array} The unique array to be worked on
-   * calls a function which checks for duplicate words
-   * @return {Array} The full unique array
+   * @param  {Array} value An array with words from jsondata
+   * eliminates duplicate words
+   * @return {Array} uniqueWords The full unique array
    */
   function eliminateDuplicateWords(value) {
-    var seperateWords = value.split(' ');
-    var uniqueWords = [];
+   var uniqueWords = value.split(' ').reduce(function (unique, word){
+        if (!unique.includes(word)){
+            unique.push(word);
+          }
 
-    return processWords(seperateWords, uniqueWords);
-  }
-
-  /**
-   * @param  {String} The words in the array
-   * @param  {Array}  The unique words in the array
-   * Checks if a word exist in the unique arrray
-   * If it exists it skips it or else pushes it
-   *  @return {Array} the unique words
-   */
-  function processWords(seperateWords, uniqueWords) {
-    for (var index = 0; index < seperateWords.length; index++) {
-      // if word has been put in array, continue (do not add)
-      if (uniqueWords.includes(seperateWords[index])) {
-        continue;
-      }
-      uniqueWords.push(seperateWords[index]);
-    }
+        return unique;
+    }, []);
 
     return uniqueWords;
   }
 
   /**
-   * @param  {String} The word to search
-   * @param  {Object}  The object with Indexing
+   * @param  {String} key The word to search
+   * @param  {Object} indexObject  The object containing words and documents found
    * manipulates search word to return the document found
-   *  @return {Array} the index o
+   *  @return {Array} searchArray the documents located
    */
-  function getIndexPosition(key, indexobject) {
+  function getIndexPosition(key, indexObject) {
     // checks if it a single word and return its position
     if ((typeof key === 'string') && key.indexOf(' ') === -1) {
-      return indexobject[key.toLowerCase()];
+      return indexObject[key.toLowerCase()];
     }
-    // splits words into one word and passes it recrsively to the method above
-    var tokens = key.split(' ');
+    // splits words into single words and returns its position
     if((typeof key === 'string') && key.indexOf(' ') !== -1) {
-      var searchArray = [];
-      for(var word = 0; word < tokens.length; word++){
-        // pushes the returned position to an array
-        searchArray.push(getIndexPosition(tokens[word], indexobject));
-      }
-      // returns the final array with elements
-      return searchArray;
-    }
+    var searchArray = key.split(' ').map(function (word){
+      return getIndexPosition(word, indexObject)
+    });
+
+    return searchArray;
+  }
   }
 
-  // validates a data is truthy
+  /**
+   * @param  {String}  data
+   * returns true if data is not found in the array
+   * @return {Boolean}
+   */
   function isValidData(data) {
     return ( ['',undefined,null,isNaN].indexOf(data) === -1)? true:false ;
   }
