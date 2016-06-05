bc656da62e5bb9cf3881258a8f45231f236005f0
Jolaade Adewale <jolaade.adewale@andela.com>
2016-06-05 19:17:41 +0100

    bug : fix title not defined

--- jasmine/spec/inverted-index-test.js
+++ jasmine/spec/inverted-index-test.js
@@ -18,8 +18,8 @@
           //asserts json file read are strings
           invertedIndex.jsonDocument.jsonFile.forEach(function (value) {
             value.map(function (obj) {
-              expect(typeof obj[title]).toEqual('string');
-              expect(typeof obj[text]).toEqual('string');
+              expect(typeof obj.title).toEqual('string');
+              expect(typeof obj.text).toEqual('string');
             });
           });
           expect(invertedIndex.documentLength).toBeGreaterThan(0);
@@ -49,18 +49,18 @@
 
   describe('Index Mapping', function () {
     it('Index are mapped to correct strings', function () {
-      expect(invertedIndex.getIndex().and).toEqual([0,1]);
+      expect(invertedIndex.getIndex().and).toEqual([0, 1]);
       expect(invertedIndex.getIndex().alice).toEqual([0]);
       expect(invertedIndex.getIndex('alice')).toEqual([0]);
       expect(invertedIndex.getIndex().seek).toEqual([1]);
-      expect(invertedIndex.getIndex().of).toEqual([0,1]);
+      expect(invertedIndex.getIndex().of).toEqual([0, 1]);
       it('Test read.json file ', function (done) {
         setTimeout (function () {
           // testing index mapping
-          expect(invertedIndex.getIndex().and).toEqual([0,1]);
+          expect(invertedIndex.getIndex().and).toEqual([0, 1]);
           expect(invertedIndex.getIndex().concise).toEqual([3]);
           expect(invertedIndex.getIndex().pocahontas).toEqual([2]);
-          expect(invertedIndex.getIndex().a).toEqual([0,1,2]);
+          expect(invertedIndex.getIndex().a).toEqual([0, 1, 2]);
           done();
         }, 500);
       });
@@ -70,24 +70,24 @@
   describe('Search Index', function () {
     it('Search index returns object with search query', function (done) {
       setTimeout (function () {
-        expect(invertedIndex.searchIndex('and')).toEqual([0,1]);
+        expect(invertedIndex.searchIndex('and')).toEqual([0, 1]);
         expect(invertedIndex.searchIndex('alice')).toEqual([0]);
         expect(invertedIndex.searchIndex('')).toEqual('');
         var pastDate = new Date();
         expect(invertedIndex.searchIndex('wonderland')).toEqual([0]);
         expect(invertedIndex.searchIndex('alice and wonderland in'))
-          .toEqual([ [ 0 ], [ 0,1 ], [ 0 ], [ 0 ] ] );
+          .toEqual([ [ 0 ], [ 0, 1 ], [ 0 ], [ 0 ] ] );
           // test complicated search works
         expect(invertedIndex.searchIndex('Wonderland ?  . concise in ' +
           '/ pocahontas, seek'))
           .toEqual([[ 0 ], [ 3 ], [ 0 ], [ 2 ], [ 1 ]] );
           // test an array passed into the search works
-        expect(invertedIndex.searchIndex([['alice', 'and', 'wonderland', 'in']]))
-          .toEqual([ [ 0 ], [ 0,1 ], [ 0 ], [ 0 ] ] );
+        expect(invertedIndex.searchIndex([['alice', 'and', 'wonderland']]))
+          .toEqual([ [ 0 ], [ 0, 1 ], [ 0 ] ] );
         expect(invertedIndex.searchIndex(['wonderland'])).toEqual([0]);
         expect(invertedIndex.searchIndex('wonderland')).toEqual([0]);
         expect(invertedIndex.searchIndex('alice and wonderland in'))
-          .toEqual([ [ 0 ], [ 0,1 ], [ 0 ], [ 0 ] ] );
+          .toEqual([ [ 0 ], [ 0, 1 ], [ 0 ], [ 0 ] ] );
         var presentDate = new  Date();
         // test time to complete 7 search's will be less than 5 milliseconds
         expect(presentDate.getMilliseconds() - pastDate.getMilliseconds())
